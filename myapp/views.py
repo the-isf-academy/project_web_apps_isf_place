605a02b2ecee5497ee848804e036609e385b1e36
Branny626 <0114019@student.isf.edu.hk>
2024-04-25 13:06:23 +0800

    Basic canvas but it repeats atm

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,11 @@ from django.utils import timezone
 from .models import Tiles
 
 def index(request):
-    Tiles_all = Tiles.objects.all()
-    num_tiles = Tiles_all.count()
-    return render(request, 'myapp/index.html', {'Tiles': Tiles_all, 'num_tiles': num_tiles})
+    grid = []
+    for x in range(1, 101):
+        row = []
+        for y in range(1, 101):
+            tile = Tiles.objects.filter(x_coordinates=x, y_coordinates=y).first()
+            row.append(tile)
+        grid.append(row)
+    return render(request, 'myapp/index.html', {'grid': grid})
