03e9356f0489130bdc2e8f1b918cc85b0bc64fd2
Branny626 <0114019@student.isf.edu.hk>
2024-05-02 10:03:17 +0800

    changes

--- myapp/models.py
+++ myapp/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Tiles(models.Model):
-    x_coordinates = models.IntegerField()
-    y_coordinates = models.IntegerField()
     r = models.IntegerField()
     g = models.IntegerField()
     b = models.IntegerField()
--- myapp/views.py
+++ myapp/views.py
@@ -4,18 +4,9 @@ from django.utils import timezone
 from .models import Tiles
 
 def index(request):
-    grid = []
-    for x in range(1, 101):
-        row = []
-        for y in range(1, 101):
-            tile = Tiles.objects.filter(x_coordinates=x, y_coordinates=y).first()
-            row.append(tile)
-        grid.append(row)
+    tiles = list(Tiles.objects.all())
+    grid = [tiles[i:i+100] for i in range(0, len(tiles), 100)]
     return render(request, 'myapp/index.html', {'grid': grid})
 
-
 def leaderboard(request):
-
-
-    
     return render(request, 'myapp/leaderboard.html',)
